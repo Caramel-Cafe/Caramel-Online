c912ddb374a960014db967218f868038259793e4
Caramel-Cafe <168911469+Caramel-Cafe@users.noreply.github.com>
2026-03-25 18:45:26 +0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,37 @@
+const menuItems = [
+  {
+    category: "Coffee",
+    name: "Espresso",
+    price: "5,000 UGX"
+  },
+  {
+    category: "Coffee",
+    name: "Cappuccino",
+    price: "7,000 UGX"
+  },
+  {
+    category: "Coffee",
+    name: "Latte",
+    price: "7,500 UGX"
+  },
+  {
+    category: "Juice",
+    name: "Mango Juice",
+    price: "6,000 UGX"
+  },
+  {
+    category: "Juice",
+    name: "Passion Juice",
+    price: "6,000 UGX"
+  },
+  {
+    category: "Food",
+    name: "Chicken Burger",
+    price: "12,000 UGX"
+  },
+  {
+    category: "Food",
+    name: "Fries",
+    price: "5,000 UGX"
+  }
+];
