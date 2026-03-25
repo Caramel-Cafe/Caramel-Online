9a6bde0689584b6adb0c3a66d442010e46eb74db
Caramel-Cafe <168911469+Caramel-Cafe@users.noreply.github.com>
2026-03-25 18:46:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const container = document.getElementById("menu");
 
+function displayMenu() {
+  let categories = {};
+
+  menuItems.forEach(item => {
+    if (!categories[item.category]) {
+      categories[item.category] = [];
+    }
+    categories[item.category].push(item);
+  });
+
+  for (let category in categories) {
+    let section = document.createElement("div");
+
+    let title = document.createElement("h2");
+    title.innerText = category;
+    section.appendChild(title);
+
+    categories[category].forEach(item => {
+      let div = document.createElement("div");
+      div.className = "menu-item";
+
+      div.innerHTML = `
+        <h3>${item.name}</h3>
+        <span class="price">${item.price}</span>
+      `;
+
+      section.appendChild(div);
+    });
+
+    container.appendChild(section);
+  }
+}
+
+displayMenu();
