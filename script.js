dc93032f7b80471717fd169153be8f34ffce2055
Caramel-Cafe <168911469+Caramel-Cafe@users.noreply.github.com>
2026-03-30 17:07:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1049,7 +1049,6 @@ function createCard(item) {
         ${blockedText ? `<p class="menu-desc" style="margin-top:8px;color:#ff9b9b;">${blockedText}</p>` : ""}
 
         <div class="menu-footer">
-          ${item.tag ? `<span class="menu-tag">${item.tag}</span>` : ""}
 
           <div class="menu-actions">
             <button
